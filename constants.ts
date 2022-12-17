ed0121ee7d754236358ad1f2b9e3acc3c6cb5699
raznorabochiy <raznorabochiy@github.com>
2022-12-17 19:53:30 +0300

    first commit

---
+++ constants.ts
@@ -0,0 +1,2 @@
+export const TELEGRAM_BOT_TOKEN = "";
+export const TELEGRAM_CHAT_ID = "";
---
+++ index.ts
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+import { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } from "./constants";
+
+function sendTelegram(text: string) {
+  const body = {
+    chat_id: TELEGRAM_CHAT_ID,
+    text,
+    parse_mode: "markdown",
+  };
+
+  return fetch(
+    `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  );
+}
+
+async function main() {
+  await sendTelegram("First message");
+  await sendTelegram("Second message _with_ *Markdown*");
+}
+
+main();
